7629e76e01d9c89fd7dfc25d9245b17319abf25b
Matti Schneider <matti.schneider@mesh-md.fr>
2012-10-08 15:25:23 +0200

    Corrected `edit` to handle hierarchies with spaces.

--- functions/edit.sh
+++ functions/edit.sh
@@ -4,13 +4,13 @@
 # Opens the given file with the set EDITOR, creating it if it doesn't exist.
 # $1 = path to file to edit
 _edit() {
-	if ! [[ -e $1 ]]
+	if ! [[ -e "$*" ]]
 	then
-		mkdir -p $(dirname $1) 2> /dev/null
-		touch $1
+		mkdir -p "$(dirname "$*")" 2> /dev/null
+		touch "$*"
 	fi
 	
-	open -a "$EDITOR" $1	
+	open -a "$EDITOR" "$*"
 }
 
 # Opens all given files with the set EDITOR, creating them if they don't exist.
