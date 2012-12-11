10d1250ca48df3b17df7dab0d7e072acd945dbdf
Matti Schneider <hi@mattischneider.fr>
2012-12-11 19:49:12 +0100

    Correct verbose

--- functions/verbose.sh
+++ functions/verbose.sh
@@ -1,6 +1,9 @@
 # Executes the passed command, and outputs its status code.
 function verbose() {	# $@: command to execute
-	local status=$("$@")
+	"$@"
+
+	local status=$?
+	
 	echo $status
 	return $status
 }
