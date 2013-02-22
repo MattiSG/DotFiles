7339290e2674888d3d86b51df624082e58ddc043
Matti Schneider <matti.schneider@mesh-md.fr>
2013-02-22 10:40:41 +0100

    Add git submodule mv prototype script

---
+++ git_submodule_mv.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+source $(dirname "$BASH_SOURCE")/../MSGShellUtils/paths.sh
+
+oldPath="$(removeTrailingSlash $1)"
+newPath="$(removeTrailingSlash $2)"
+
+if ! [[ -d "$oldPath" ]]
+then
+	echo "'$oldPath' is not a directory"
+	echo
+	echo "Usage: $(basename $0) <oldPath> <newPath>"	# TODO: check that <newPath> does not exist yet
+	exit 1
+fi
+
+# Credit: http://stackoverflow.com/a/13942859/594053
+originUrl=$(git config --local --get submodule.$oldPath.url)
+
+## add new submodule
+mkdir -p $(dirname "$newPath")	# TODO: add && everywhere (use staged_process?)
+git submodule add "$originUrl" "$newPath"	# TODO: copy the commit ID of the old submodule
+# TODO: move existing instead of cloning the whole repo (see StackOverflow answer)
+
+## remove old submodule
+git config -f .git/config --remove-section "submodule.$oldPath"
+git config -f .gitmodules --remove-section "submodule.$oldPath"
+git rm --cached "$oldPath"
+rm -rf "$oldPath"	# remove old src
+rm -rf ".git/modules/$oldPath"	# cleanup gitdir
+
+git add .gitmodules
+
+echo "Done!"
+echo
+echo "Move $(basename $oldPath) submodule"	# TODO: prepare commit message
