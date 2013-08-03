a257bc980a5745e1a087e6b29a8d99f475db1e59
Matti Schneider <hi@mattischneider.fr>
2013-08-03 16:20:05 +0200

    Add gems path for Ruby installed with brew

--- paths.sh
+++ paths.sh
@@ -1,12 +1,18 @@
-#Homebrew
+# Homebrew
 export PATH=/usr/local/sbin:$PATH
 
-#Node
+# Node
 export PATH=/usr/local/bin:$PATH
 
-#Ensure globally-installed modules can be required without npm link
-#Source: http://www.davidarno.org/2012/02/09/writing-a-node.js-module-in-coffeescript/
+# Ensure globally-installed modules can be required without npm link
+# Source: http://www.davidarno.org/2012/02/09/writing-a-node.js-module-in-coffeescript/
 export NODE_PATH=/usr/local/lib/node_modules/
 
+# DevTools
 export PATH=/Developer/Tools:$PATH
+
+# Java
 export CLASSPATH=.:${CLASSPATH}
+
+# Ruby with Brew
+export PATH=/usr/local/opt/ruby/bin:$PATH
