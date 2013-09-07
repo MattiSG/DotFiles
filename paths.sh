ca3922bbb3f5dda7d3122fe959c26d44e07feb99
Matti Schneider <hi@mattischneider.fr>
2013-09-07 16:32:29 +0200

    Add NPM global path to PATH

--- paths.sh
+++ paths.sh
@@ -4,6 +4,9 @@ export PATH=/usr/local/sbin:$PATH
 # Node
 export PATH=/usr/local/bin:$PATH
 
+# NPM
+export PATH=/usr/local/share/npm/bin:$PATH
+
 # Ensure globally-installed modules can be required without npm link
 # Source: http://www.davidarno.org/2012/02/09/writing-a-node.js-module-in-coffeescript/
 export NODE_PATH=/usr/local/lib/node_modules/
