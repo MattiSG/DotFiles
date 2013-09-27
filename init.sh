5b3d33f817fa6633374b1aa3ffa7b21a2dc8d054
Matti Schneider <matti.schneider@mesh-md.fr>
2013-09-27 11:16:04 +0200

    Add support for linking nested dotfiles

--- init.sh
+++ init.sh
@@ -1,12 +1,18 @@
 cd $(dirname "$BASH_SOURCE")
+source="$(pwd)"
 
 git submodule sync
 git submodule update --init --recursive
 
-source=$(pwd)
+for file in $(find dots -type f)
+do
+	dotfile=$(echo $file | cut -d '/' -f 2-)	# remove "dots/" directory
+	target="$HOME/.$dotfile"
 
-for file in $(ls "$source/dots")
-do ln -s "$source/$file" ~/.$file
+	if [[ `readlink "$target"` == "$dotfile" ]]
+	then echo "$target already linked"
+	else ln -s -v "$source/dots/$dotfile" "$target"
+	fi
 done
 
 echo "source $source/profile" > ~/.profile
