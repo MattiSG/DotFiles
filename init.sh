fb92539daa14065d4bb9d7f962fafd68158d03b2
Matti <hi@mattischneider.fr>
2013-12-23 23:48:21 +0100

    Improve dotfiles linking
    Uniform script with other handlers

--- init.sh
+++ init.sh
@@ -4,17 +4,6 @@ source="$(pwd)"
 git submodule sync
 git submodule update --init --recursive
 
-for file in $(find dots -type f)
-do
-	dotfile=$(echo $file | cut -d '/' -f 2-)	# remove "dots/" directory
-	target="$HOME/.$dotfile"
-
-	if [[ `readlink "$target"` == "$dotfile" ]]
-	then echo "$target already linked"
-	else ln -s -v "$source/dots/$dotfile" "$target"
-	fi
-done
-
 if [[ -f ~/.profile ]]
 then echo ".profile already exists. Refusing to overwrite it."
 else
---
+++ session/link-dotfiles.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+# Symlinks all dotfiles into user's home dir.
+
+cd "$(dirname "$BASH_SOURCE")/../dots"
+DOTFILES_DIR="$(pwd)"
+
+actionForAllFiles=''
+
+function link() {	# $1 = source file; $2 = target file
+	local source="$1"
+	local target="$2"
+
+	local action=$actionForAllFiles
+	local skip=''
+
+	if [[ `readlink "$target"` == "$source" ]]
+	then
+		echo "$target already linked"
+		skip=true
+	elif [[ -e "$target" ]] || [[ -h "$target" ]]
+	then
+		if [[ -z $actionForAllFiles ]]
+		then
+			read -p "File '$target' already exists. [s]kip (default), [S]kip all, [o]verwrite, [O]verwrite all, [b]ackup, [B]ackup all?" -n 1 action
+			echo
+		fi
+
+		case $action in
+			O) actionForAllFiles='o';;
+			B) actionForAllFiles='b';;
+			S) actionForAllFiles='s';;
+		esac
+
+		case ${actionForAllFiles:-$action} in
+			o) rm "$target";;
+			b) mv -v "$target" "$target~";;	# TODO: doesn't handle what happens if a backup already exists
+			*) skip=true;;
+		esac
+	fi
+
+	if [[ -z $skip ]]
+	then ln -s -v "$source" "$target"
+	fi
+}
+
+cd ..
+
+for file in $(find dots -type f)
+do
+	dotfile=$(echo $file | cut -d '/' -f 2-)	# remove "dots/" directory
+	link "$DOTFILES_DIR/$dotfile" "$HOME/.$dotfile"
+done
