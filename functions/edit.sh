0d7c8f08703d7b5ffd3d823ae0c03a04f2213974
Matti Schneider <hi@mattischneider.fr>
2013-12-25 18:05:32 +0100

    Support symlinks in `edit` shortcut

--- functions/edit.sh
+++ functions/edit.sh
@@ -4,13 +4,17 @@
 # Opens the given file with the set EDITOR, creating it if it doesn't exist.
 # $1 = path to file to edit
 _edit() {
-	if ! [[ -e "$*" ]]
+	target="$*"	# allow unprotected spaces
+
+	if ! [[ -e "$target" ]]
 	then
-		mkdir -p "$(dirname "$*")" 2> /dev/null
-		touch "$*"
+		mkdir -p "$(dirname "$target")" 2> /dev/null
+		touch "$target"
+	elif [ -h "$target" ]
+	then target=`readlink "$target"`
 	fi
 
-	open -a "Sublime Text 2" "$*"
+	open -a "Sublime Text 2" "$target"
 }
 
 # Opens all given files with the set EDITOR, creating them if they don't exist.
