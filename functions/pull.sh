a9acfbc496da76d1a8b66092779a4cb4af69d654
Matti Schneider <matti@mesh-md.fr>
2014-02-18 10:33:33 +0100

    Minor doc improvement

--- functions/pull.sh
+++ functions/pull.sh
@@ -1,6 +1,7 @@
 # Pulls from a Git repo, updating all submodules.
 
-function pull() {	# [options to pass to `git pull`
+# $@ = options to pass to `git pull`
+function pull() {
 	git pull "$@" &&
 	git submodule sync &&
 	git submodule update --init --recursive
