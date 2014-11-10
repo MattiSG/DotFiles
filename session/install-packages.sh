054beb06c87ae2151465aac0f6558f0f39b0329b
Matti Schneider <hi@mattischneider.fr>
2014-11-10 11:29:46 +0100

    Support installing Package Control on new session

--- session/install-packages.sh
+++ session/install-packages.sh
@@ -1,2 +1,5 @@
+DEST_DIR="~/Library/Application Support/Sublime Text 2/Installed Packages"
+
 echo 'Installing Sublime Text Package Control…'
-curl -# https://sublime.wbond.net/Package%20Control.sublime-package -o ~/Library/Application\ Support/Sublime\ Text\ 2/Installed\ Packages/Package\ Control.sublime-package
+mkdir -p "$DEST_DIR"
+curl -# https://sublime.wbond.net/Package%20Control.sublime-package -o "$DEST_DIR/Package Control.sublime-package"
