f0ee3354ab568190f05eec28e0d65aff74516b59
Matti Schneider <matti.schneider@data.gouv.fr>
2015-05-20 17:01:12 +0200

    Fix paths precedence
    Ensure NPM-installed is used over brew Node-packaged NPM

--- source/paths.sh
+++ source/paths.sh
@@ -5,7 +5,7 @@ export PATH=/usr/local/sbin:$PATH
 export PATH=/usr/local/bin:$PATH
 
 # NPM
-export PATH=/usr/local/share/npm/bin:$PATH
+export PATH=$PATH:/usr/local/share/npm/bin
 
 # Ensure globally-installed modules can be required without npm link
 # Source: http://www.davidarno.org/2012/02/09/writing-a-node.js-module-in-coffeescript/
@@ -13,13 +13,13 @@ export NODE_PATH=/usr/local/lib/node_modules/
 
 # Go
 export GOPATH=/usr/local/Cellar/share/go/bin
-export PATH=$GOPATH/bin:$PATH
+export PATH=$PATH:$GOPATH/bin
 
 # DevTools
-export PATH=/Developer/Tools:$PATH
+export PATH=$PATH:/Developer/Tools
 
 # Java
 export CLASSPATH=.:${CLASSPATH}
 
 # Ruby with Brew
-export PATH=/usr/local/opt/ruby/bin:$PATH
+export PATH=$PATH:/usr/local/opt/ruby/bin
