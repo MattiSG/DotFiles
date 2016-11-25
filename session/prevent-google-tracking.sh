b1420d12bf808f0c96edb096549c71038cf90fd1
Matti Schneider <matti.schneider@beta.gouv.fr>
2016-11-25 16:20:41 +0100

    Improve Google tracking prevention UX

--- session/prevent-google-tracking.sh
+++ session/prevent-google-tracking.sh
@@ -1,10 +1,16 @@
 # Credit: https://raamdev.com/2008/howto-remove-google-software-update-on-mac-os-x/
 
-# Remove Google Software Update
 if [[ -d ~/Library/Google/GoogleSoftwareUpdate ]]
-then ~/Library/Google/GoogleSoftwareUpdate/GoogleSoftwareUpdate.bundle/Contents/Resources/ksinstall --nuke
+then
+	~/Library/Google/GoogleSoftwareUpdate/GoogleSoftwareUpdate.bundle/Contents/Resources/ksinstall --nuke
+	echo "Removed Google Software Update"
 fi
 
-# Prevent reinstalls
-touch ~/Library/Google/GoogleSoftwareUpdate
-sudo chown nobody:nobody ~/Library/Google/GoogleSoftwareUpdate
+if ! touch ~/Library/Google/GoogleSoftwareUpdate 2> /dev/null
+then
+	echo "Installation of Google Software Update already prevented"
+else
+	echo "Requesting access to prevent future installations of Google Software Update…"
+	sudo chown nobody:nobody ~/Library/Google/GoogleSoftwareUpdate
+	echo "done"
+fi
