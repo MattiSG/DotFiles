de8d1123a8b2a015566838fc2414104ff74b11b0
Matti Schneider <hi@mattischneider.fr>
2017-10-29 17:51:53 +0800

    Document proxy

--- functions/proxy.sh
+++ functions/proxy.sh
@@ -1,5 +1,9 @@
-TUNNEL_PORT=2465
-SERVER='root@139.99.105.193'
+# Proxy a network interface on macOS to a server through an SSH tunnel
+
+TUNNEL_PORT=2465  # set this to an available port on your machine
+SERVER='root@139.99.105.193'  # set this to a server on which you can SSH
+INTERFACE='Wi-Fi'  # set this to the name of the interface to proxy; you can get this name in the Network pane of System Preferences
+REMOTE_PORT=443  # set this to a port on which you set up sshd to listen to on the proxy server; 443 is disabled by default, but using this port circumvents proxy prevention in countries that filter internet access, since traffic is disguised as HTTPS
 
 function tunnel_pid() {
 	ps aux | grep $TUNNEL_PORT | grep ssh | tr -s ' ' | cut -d ' ' -f 2
@@ -15,13 +19,11 @@ function proxy() {
 		return 1
 	fi
 
-	local interface="Wi-Fi"
-
 	if [[ $1 = 'on' ]]
 	then
-		ssh -Nf -D $TUNNEL_PORT $SERVER
+		ssh -Nf -D $TUNNEL_PORT $SERVER -p $REMOTE_PORT
 		echo "Tunnel PID: $(tunnel_pid)"
-		sudo networksetup -setsocksfirewallproxy "$interface" localhost $TUNNEL_PORT
+		sudo networksetup -setsocksfirewallproxy "$INTERFACE" localhost $TUNNEL_PORT
 	elif [[ $1 = 'off' ]]
 	then
 		if [[ $(tunnel_pid) ]]
@@ -29,9 +31,9 @@ function proxy() {
 			kill $(tunnel_pid)
 			echo "Tunnel stopped"
 		fi
-		sudo networksetup -setsocksfirewallproxystate "$interface" off
+		sudo networksetup -setsocksfirewallproxystate "$INTERFACE" off
 	fi
 
-	networksetup -getsocksfirewallproxy "$interface"
+	networksetup -getsocksfirewallproxy "$INTERFACE"
 	echo "External IP: $(ip)"
 }
