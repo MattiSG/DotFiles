9dd1bc468f401f2393720145e9b5fc81257e788e
Matti Schneider <hi@mattischneider.fr>
2017-10-29 18:04:41 +0800

    Improve proxy documentation

--- functions/proxy.sh
+++ functions/proxy.sh
@@ -3,7 +3,7 @@
 TUNNEL_PORT=2465  # set this to an available port on your machine
 TUNNEL_SERVER='root@139.99.105.193'  # set this to a server on which you can SSH
 TUNNEL_INTERFACE='Wi-Fi'  # set this to the name of the interface to proxy; you can get this name in the Network pane of System Preferences
-TUNNEL_REMOTE_PORT=443  # set this to a port on which you set up sshd to listen to on the proxy server; 443 is disabled by default, but using this port circumvents proxy prevention in countries that filter internet access, since traffic is disguised as HTTPS
+TUNNEL_REMOTE_PORT=443  # set this to a port on which you set up sshd to listen to on the proxy server; 443 is disabled by default, but using this port circumvents proxy prevention in countries that filter internet access, since traffic is disguised as HTTPS. See how to set up these ports on https://help.ubuntu.com/community/SSH/OpenSSH/Configuring#Troubleshooting.
 
 function tunnel_pid() {
 	ps aux | grep $TUNNEL_PORT | grep ssh | tr -s ' ' | cut -d ' ' -f 2
