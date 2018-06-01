bff046a36bd0b72e0546c3a0f51506845995feeb
Matti Schneider <labplus@matti.consulting>
2018-06-01 12:39:23 +1200

    Use Wi-Fi as default local IP source

--- source/aliases.sh
+++ source/aliases.sh
@@ -53,7 +53,7 @@ alias sudo="sudo "
 
 # IP addresses
 alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
-alias localip="ipconfig getifaddr en1"
+alias localip="ifconfig getifaddr en0"
 alias ips="ifconfig -a | grep -o 'inet6\? \(\([0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+\)\|[a-fA-F0-9:]\+\)' | sed -e 's/inet6* //'"
 
 # Enhanced WHOIS lookups
