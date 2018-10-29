5061a632cce121c37eb6379042b131a45a5bc177
Matti Schneider <hi@mattischneider.fr>
2018-10-29 11:08:59 +0100

    Add --dry-run option to reencode.sh

--- reencode.sh
+++ reencode.sh
@@ -1,14 +1,25 @@
-cd `dirname "$1"`
+#!/bin/bash
 
-TARGET_DIR_PREFIX="${2:-Reencoded}"
+if [[ $1 == '--dry-run' ]]
+then
+	dry_run=true
+	shift
+fi
+
+if [[ $# == 0 ]]
+then source_files="*.[mM][pPoO4][4gGvV]"
+else source_files="$@"
+fi
+
+
+TARGET_DIR_PREFIX="Reencoded"
+
 encoder=libx265  # libx265 = HEVC, little support but much smaller filesize; libx264 = H.264, very wide support
 quality=28  # ok quality = 28 for HEVC, 23 for H264; great quality = 22 for HEVC, 20 for H264
 scale=""  # decrease size of full HD streams: set to 1280:720; leave empty for no downscaling
 audio_bitrate="80k"  # decrease audio quality to something acceptable for voice; set to 180k for precise music; leave empty to remove audio
 
 target_dir="$TARGET_DIR_PREFIX-$encoder-$quality"
-
-mkdir "$target_dir"
 
 shared_options=""
 shared_options="$shared_options -crf $quality"
@@ -28,22 +39,30 @@ else shared_options="$shared_options -an"
 fi
 shared_options="$shared_options -movflags +faststart"  # ensure internet streaming is smooth
 
-reencode() {
+reencode_command() {
 	filename=`echo "$1" | rev | cut -d '.' -f 2- | rev`  # remove extension
 
 	if [[ -e "$filename.jpg" ]]
 	then local file_specific_options="-attach $filename.jpg"  # add cover artwork
 	fi
 
-	ffmpeg -i "$1" $shared_options $file_specific_options "$target_dir/$filename.$encoder.mp4"
-	touch -r "$1" "$target_dir/$filename.$encoder.mp4"  # copy mtime and ctime
+	echo ffmpeg -i "$1" $shared_options $file_specific_options "$target_dir/$filename.$encoder.mp4"
 }
 
-if [[ $1 ]]
-then reencode "$1"
-else
-	for file in *.[mM][pPoO4][4gGvV]
-	do reencode "$file"
-	done
+
+if ! [[ $dry_run ]]
+then mkdir "$target_dir"
 fi
+
+for file in $source_files
+do
+	command=`reencode_command "$file"`
+
+	if [[ $dry_run ]]
+	then echo $command
+	else
+		$command
+		touch -r "$1" "$target_dir/$filename.$encoder.mp4"  # copy mtime and ctime
+	fi
+done
 
