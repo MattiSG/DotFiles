b439d8f9795e9c8f84a9d1986bd39f9a0e3f37b3
Matti Schneider <hi@mattischneider.fr>
2019-01-08 18:12:03 +0100

    Don't show recent apps in Dock
    Mojave new feature

--- session/osx-defaults.sh
+++ session/osx-defaults.sh
@@ -235,6 +235,10 @@ unset file
 # Dock & hot corners                                                          #
 ###############################################################################
 
+defaults write com.apple.dock orientation left
+
+defaults write com.apple.dock show-recents -bool false
+
 # Enable highlight hover effect for the grid view of a stack (Dock)
 defaults write com.apple.dock mouse-over-hilite-stack -bool true
 
