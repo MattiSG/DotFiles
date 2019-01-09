fc1802d28638b29112da2859d99009a82ff25035
Matti Schneider <hi@mattischneider.fr>
2019-01-09 10:16:11 +0100

    Automatically switch default Terminal theme

--- session/osx-defaults.sh
+++ session/osx-defaults.sh
@@ -417,15 +417,10 @@ defaults write com.apple.iTunes disableRadio -bool true
 defaults write com.apple.terminal StringEncodings -array 4
 
 # Use a modified version of the Pro theme by default in Terminal.app
-#open "$HOME/init/Mathias.terminal"
-#sleep 1 # Wait a bit to make sure the theme is loaded
-#defaults write com.apple.Terminal "Default Window Settings" -string "Mathias"
-#defaults write com.apple.Terminal "Startup Window Settings" -string "Mathias"
-
-# Enable “focus follows mouse” for Terminal.app and all X11 apps
-# i.e. hover over a window and start typing in it without clicking first
-#defaults write com.apple.terminal FocusFollowsMouse -bool true
-#defaults write org.x.X11 wm_ffm -bool true
+open "`dirname $BASH_SOURCE`/../Library/Terminal Settings/Matti.terminal"
+sleep 1 # Wait a bit to make sure the theme is loaded
+defaults write com.apple.Terminal "Default Window Settings" -string "Matti"
+defaults write com.apple.Terminal "Startup Window Settings" -string "Matti"
 
 ###############################################################################
 # Time Machine                                                                #
