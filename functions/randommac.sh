42568c079d442e6589643e088ec92c9170ceb7d6
Matti Schneider <gh@mattischneider.fr>
2020-07-20 15:55:34 +0200

    Make randommac verbose
    Add mymac function

--- functions/randommac.sh
+++ functions/randommac.sh
@@ -1,4 +1,13 @@
 # Credit: @anthonyheddings
 function randommac() { # [$1 = interface to set up a random MAC address for, defaults to en0]
-	openssl rand -hex 6 | sed 's/\(..\)/\1:/g; s/.$//' | xargs sudo ifconfig ${1:-en0} ether
+	interface=${1:-en0}
+	echo "Previous MAC address for $cyanf$interface$reset was $redf`mymac $interface`$reset"
+
+	openssl rand -hex 6 | sed 's/\(..\)/\1:/g; s/.$//' | xargs sudo ifconfig $interface ether
+
+	echo "New MAC address for $cyanf$interface$reset is $greenf`mymac $interface`$reset"
+}
+
+function mymac() {
+	ifconfig ${1:-en0} | grep ether | cut -d ' ' -f 2
 }
