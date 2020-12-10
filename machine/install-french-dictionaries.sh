eb86258e1daf79b7e7950b072dc355af5a128e61
Matti Schneider <gh@mattischneider.fr>
2020-12-10 18:42:47 +0100

    Fix permissions

--- machine/install-french-dictionaries.sh
+++ machine/install-french-dictionaries.sh
@@ -5,12 +5,12 @@ sudo -v
 mkdir -p "$TARGET_DIR"
 
 install_dictionary() {
-	curl -# "https://dl.mattischneider.fr/dictionaries/$1.dictionary.zip" -o "$TARGET_DIR/$1.dictionary.zip"
-	unzip -o "$TARGET_DIR/$1.dictionary.zip" -x '__MACOSX/*' -d "$TARGET_DIR/"
-	rm "$TARGET_DIR/$1.dictionary.zip"
+	sudo curl -# "https://dl.mattischneider.fr/dictionaries/$1.dictionary.zip" -o "$TARGET_DIR/$1.dictionary.zip"
+	sudo unzip -o "$TARGET_DIR/$1.dictionary.zip" -x '__MACOSX/*' -d "$TARGET_DIR/"
+	sudo rm "$TARGET_DIR/$1.dictionary.zip"
 }
 
 install_dictionary xmlittre
 install_dictionary academie-francaise
 
-chown -R root:wheel "$TARGET_DIR"
+sudo chown -R root:wheel "$TARGET_DIR"
